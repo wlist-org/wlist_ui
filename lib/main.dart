c7483bc61fee59712978dfb8344603617938c5c2
xuxiaocheng <2762267080@qq.com>
2024-07-20 15:49:54 +0800

    feat(window): 设置窗口最小大小

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:wlist_ui/pages/login.dart';
 
 import 'generated/l10n.dart';
 
-void main() {
+void main() async {
   runApp(const WlistApp());
+  try {
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      WidgetsFlutterBinding.ensureInitialized();
+      await windowManager.ensureInitialized();
+      WindowOptions windowOptions = WindowOptions(
+        size: const Size(800, 600),
+        center: true,
+        minimumSize: const Size(400, 600),
+        title: S.current.title,
+      );
+      windowManager.waitUntilReadyToShow(windowOptions, () async {
+        await windowManager.show();
+        await windowManager.focus();
+      });
+    }
+  } catch (_) {
+  }
 }
 
 class WlistApp extends StatelessWidget {
